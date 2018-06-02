4e32b24a5340f06147567212b3c60957cbdbf21b
run1t <reunanln@gmail.com>
2018-06-02 20:31:31 +0200

    Use child component to improve performances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,9 @@ import { AppComponent } from './app.component';
 import { EmployeeListModule } from './employee-list/employee-list.module';
 
 import { ListGenerator } from './shared/list-generator.service';
-<<<<<<< HEAD
+
 import { EmployeeService } from './shared/employee.service';
-=======
->>>>>>> 687f2b9e864c16bda345009c42a724ca42c91b08
+
 
 @NgModule({
   imports: [BrowserModule, EmployeeListModule],
